7e85b01638252736be1a1cb574e3efae6c2ce196
hiago <hiago_s93@hotmail.com>
2017-12-06 00:02:41 -0300

    fk off

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Rondonpe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     #config.secret_key = ENV['SECRET_KEY_BASE']
-    config.DATABASE_URL = ENV['CLEARDB_DATABASE_URL']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
